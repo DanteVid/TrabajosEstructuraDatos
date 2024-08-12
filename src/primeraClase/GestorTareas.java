d5e05de3cef97fdee384a8efdef7fbe24b5c3a2a
unknown <daniel.ruiz@estudiantesunibague.edu.co>
2024-08-11 19:34:06 -0500

    Terminado

--- src/primeraClase/GestorTareas.java
+++ src/primeraClase/GestorTareas.java
@@ -1,37 +1,73 @@
 package primeraClase;
 
+import java.util.Objects;
+
 public class GestorTareas {
     private Tarea cab;
-
 
 
     public void anadirTarea(Tarea nuevaTarea) {
         if (cab == null) {
             cab = nuevaTarea;
+        } else if (buscarTarea(nuevaTarea.getDescripcionTarea())){
+            System.out.println("Ya está en la lista, intenta con otro");
+            //Verificar si está o no, si ya está, tira el mensaje de error y no hace nada
+        } else {
+            Tarea rec = cab;
+            while(rec.getSgte()!=null){
+                rec = rec.getSgte();
+            }
+            rec.setSgte(nuevaTarea);
+        }
+    }
+
+    public void printTareas() {
+        if (cab == null) {
+            System.out.println("¡La lista está vacía!");
             return;
         } else {
-            //Recorrer la lista para verificar si está o no la tarea/nombre
             Tarea rec = cab;
-            while (rec.getSgte() != null){
-                if (rec.getDescripcionTarea().equalsIgnoreCase(nuevaTarea.getDescripcionTarea())){
-                    System.out.println("¡La tarea ya está en la lista!");
-                    return;
-                }
+            while (rec != null) {
+                System.out.println(rec);
                 rec = rec.getSgte();
             }
         }
     }
 
-    public void printTareas(){
+    public void eliminarTarea(String descripcionTareaBuscada) {
         if (cab == null) {
             System.out.println("¡La lista está vacía!");
-            return;
-        }else {
+        } else if (cab.getDescripcionTarea().equalsIgnoreCase(descripcionTareaBuscada)) {
+            cab = cab.getSgte();
+            System.out.println("Tarea/nombre borrad@ con éxito");
+        } else {
+            Tarea rec = cab.getSgte();
+            Tarea ant = cab;
+            while (rec != null) {
+                if (rec.getDescripcionTarea().equalsIgnoreCase(descripcionTareaBuscada)) {
+                    ant.setSgte(rec.getSgte());
+                    System.out.println("Tarea/nombre borrad@ con éxito");
+                    return;
+                }
+                ant = rec;
+                rec = rec.getSgte();
+            }
+            System.out.println("Tarea/nombre no está en la lista");
+        }
+    }
+
+    public boolean buscarTarea(String descripcionTareaBuscada) {
+        if (cab == null) {
+            System.out.println("¡La lista está vacía!");
+        } else {
             Tarea rec = cab;
-            while (rec != null){
-                System.out.println(rec);
+            while (rec != null) {
+                if (rec.getDescripcionTarea().equalsIgnoreCase(descripcionTareaBuscada)) {
+                    return true;
+                }
                 rec = rec.getSgte();
             }
         }
+        return false;
     }
 }
--- src/primeraClase/Main.java
+++ src/primeraClase/Main.java
@@ -8,10 +8,10 @@ public class Main {
         Tarea tarea;
         GestorTareas gestorTareas = new GestorTareas();
         Scanner scanner = new Scanner(System.in);
-
+        String descripcionTareaBuscada = "";
         while (true) {
             int op = 0;
-            System.out.println("Bienvenido, por favor seleccione una opción:");
+            System.out.println("Por favor elige una opción:");
             System.out.println("1. Agregar un nombre/tarea (sólo podrás hacerlo si no está presente en la lista)");
             System.out.println("2. Mostrar todos los nombres o tareas pendientes");
             System.out.println("3. Buscar un nombre/tarea para verificar si está en la lista");
@@ -27,13 +27,28 @@ public class Main {
                     gestorTareas.anadirTarea(tarea);
                     break;
                 case 2:
+                    System.out.println();
+                    System.out.println("Las tareas pendientes/nombres que hay en la lista son:");
                     gestorTareas.printTareas();
+                    System.out.println();
                     break;
                 case 3:
-
+                    System.out.println("Ingresa la tarea o nombre que quieres verificar:");
+                    descripcionTareaBuscada = scanner.nextLine();
+                    if (gestorTareas.buscarTarea(descripcionTareaBuscada)){
+                        System.out.println("La tarea/nombre ya se encuentra en la lista");
+                    }else {
+                        System.out.println("La tarea/nombre no se encuentra en la lista");
+                    }
+                    System.out.println();
                     break;
                 case 4:
-
+                    System.out.println("Ingresa la tarea o nombre que quieres borrar:");
+                    descripcionTareaBuscada = scanner.nextLine();
+                    gestorTareas.eliminarTarea(descripcionTareaBuscada);
+                    break;
+                case 5:
+                    System.exit(0);
                     break;
                 default:
                     System.out.println("Eligiste una opción incorrecta");
