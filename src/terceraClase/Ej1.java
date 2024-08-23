be494e4bfa37f5b2a5e7f9b2ab920af627fba471
DanteVid <daniel.ruiz@estudiantesunibague.edu.co>
2024-08-23 14:56:20 -0500

    FaltaPorTerminar.java

--- src/terceraClase/Ej1.java
+++ src/terceraClase/Ej1.java
@@ -2,13 +2,6 @@ package terceraClase;
 
 public class Ej1 {
 
-    //Complejidad temporal:
-    public void printFirstElement(int[] arr) { // O(1) (Declara una variable en los atributos)
-        if (arr.length > 0) { //(O(1)
-            System.out.println(arr[0]); //O(1)
-        }
-    }
-
     // O(1)*O(1)*O(1)=O(1) -> O(1)
 
     //Complejidad espacial:
