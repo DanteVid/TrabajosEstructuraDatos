c2ca5bce452e10a8831087be29e285ca0856b8b9
DanteVid <daniel.ruiz@estudiantesunibague.edu.co>
2024-09-06 16:26:52 -0500

    Metodo Agregar

--- src/quintaClase/ListaCircularDobEnlazada.java
+++ src/quintaClase/ListaCircularDobEnlazada.java
@@ -5,5 +5,20 @@ import cuartaClase.Nodo;
 public class ListaCircularDobEnlazada {
     private Nodo cab;
     private Nodo cola;
+    
+    public void agregarNodo(int num) {
+        Nodo nuevo = new Nodo(num);
+        if (cabeza == null) {
+            cabeza = nuevoNodo;
+            cola = nuevoNodo;
+            cabeza.setSgte(cabeza);
+            cabeza.setPrev(cabeza);
+        } else {
+            cola.setSgte(nuevoNodo);
+            cabeza.setPrev(nuevoNodo);
+            cola = nuevoNodo;
+            cola.setSgte(cabeza);
+        }
+    }
 
 }
