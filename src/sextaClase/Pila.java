f1d1a5954de425ff699e1a02215ac17051467749
DanteVid <daniel.ruiz@estudiantesunibague.edu.co>
2024-09-20 07:31:13 -0500

    Terminado Ej1

---
+++ src/sextaClase/Pila.java
@@ -0,0 +1,34 @@
+package sextaClase;
+import cuartaClase.Nodo;
+public class Pila {
+    private Nodo top;
+    private Nodo bottom;
+
+    public Nodo getTop() {
+        return top;
+    }
+
+    public void setTop(Nodo top) {
+        this.top = top;
+    }
+
+    public void push(char a){
+        Nodo nuevo = new Nodo(a);
+        if (top == null){
+            top = nuevo;
+            bottom = nuevo;
+        }else {
+            nuevo.setSgte(top);
+            top = nuevo;
+        }
+    }
+
+    public void pop(){
+        if (top == null){
+            System.out.println("Pila vacía, está mal la expresión");
+            System.exit(0);
+        }else {
+            top = top.getSgte();
+        }
+    }
+}
